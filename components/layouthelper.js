9da71ca2140c8ed2ab1970b4df1912a7128cd742
CyFist <89368887+CyFist@users.noreply.github.com>
2023-04-26 12:49:02 +0800

    Remove emtioncache and update CssVarsProvider

--- components/layouthelper.js
+++ components/layouthelper.js
@@ -1,21 +1,14 @@
 "use client";
 
 import { RecoilRoot } from "recoil";
-import AppThemeProvider from "../theme/ThemeProvider";
+import AppThemeProvider from "../theme/CssVarsProvider";
 import CssBaseline from "@mui/material/CssBaseline";
 import Layout from "../components/layout";
-
-import createEmotionCache from "../theme/createEmotionCache";
-import { CacheProvider } from "@emotion/react";
-
-const clientSideEmotionCache = createEmotionCache();
 
 export default function Layouthelper({ children }) {
   return (
     <RecoilRoot>
-      <CacheProvider value={clientSideEmotionCache}>
         <AppThemeProvider>{children}</AppThemeProvider>
-      </CacheProvider>
     </RecoilRoot>
   );
 }
